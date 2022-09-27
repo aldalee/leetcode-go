45aea9fd9721afb2ddd3d99f796569604f10bdb3
HuanyuLee <hadoop_2020@163.com>
2022-09-27 20:20:53 +0800

    70.爬楼梯

---
+++ src/dp/Code0070_climbStairs.go
@@ -0,0 +1,15 @@
+// https://leetcode.cn/problems/climbing-stairs/
+package main
+
+func climbStairs(n int) int {
+	p, q, r := 0, 0, 1
+	for i := 1; i <= n; i++ {
+		p = q
+		q = r
+		r = p + q
+	}
+	return r
+}
+func main() {
+
+}
