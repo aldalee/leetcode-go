b242d35bf3a5bf94ab7c22a8b08ae85e0baf6c02
aldalee <hadoop_2020@163.com>
2023-03-18 22:16:04 +0800

    34.在排序数组中查找元素的第一个和最后一个位置

--- src/array/code0034_find_first_and_last_position_of_element_in_sorted_array.go
+++ src/array/code0034_find_first_and_last_position_of_element_in_sorted_array.go
@@ -1,58 +1,13 @@
 // https://leetcode.cn/problems/find-first-and-last-position-of-element-in-sorted-array/
 package main
 
-import "math"
+import "sort"
 
 func searchRange(nums []int, target int) []int {
-	l, r, mid := binarySearch(nums, target)
-	if mid == -1 {
+	L := sort.SearchInts(nums, target)
+	if L == len(nums) || nums[L] != target {
 		return []int{-1, -1}
 	}
-	start := binarySearchLeft(nums, l, mid, target)
-	end := binarySearchRight(nums, mid, r, target)
-	return []int{start, end}
-}
-
-func binarySearch(nums []int, target int) (l int, r int, index int) {
-	l, r = 0, len(nums)-1
-	for l <= r {
-		mid := ((r - l) >> 1) + l
-		if nums[mid] == target {
-			return l, r, mid
-		}
-		if nums[mid] > target {
-			r = mid - 1
-		} else {
-			l = mid + 1
-		}
-	}
-	return l, r, -1
-}
-
-func binarySearchLeft(nums []int, l int, r int, target int) int {
-	index := int(math.Max(float64(l), float64(r)))
-	for l <= r {
-		mid := ((r - l) >> 1) + l
-		if nums[mid] == target {
-			index = mid
-			r = mid - 1
-		} else if nums[mid] < target {
-			l = mid + 1
-		}
-	}
-	return index
-}
-
-func binarySearchRight(nums []int, l int, r int, target int) int {
-	index := int(math.Min(float64(l), float64(r)))
-	for l <= r {
-		mid := ((r - l) >> 1) + l
-		if nums[mid] == target {
-			index = mid
-			l = mid + 1
-		} else if nums[mid] > target {
-			r = mid - 1
-		}
-	}
-	return index
+	R := sort.SearchInts(nums, target+1) - 1
+	return []int{L, R}
 }
